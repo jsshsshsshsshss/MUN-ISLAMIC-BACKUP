7d046519224ee14b82102c346fcf0b889e55cd60
BOT-BACKUP <md_khairul_islam_mosaddek@yahoo.com>
2025-03-16 04:24:46 +0600

    Update ｢File｣__prefix.js

--- Script/commands/｢File｣__prefix.js
+++ Script/commands/｢File｣__prefix.js
@@ -25,10 +25,10 @@ module.exports.handleEvent = async ({ event, api, Threads }) => {
     if (body === i.toUpperCase() | body === i | str === body) {
 const prefix = threadSetting.PREFIX || global.config.PREFIX;
       if (data.PREFIX == null) {
-        return out(`This Is My Prefix ⇉ [ ${prefix} ]\n\n𝐎𝐖𝐍𝐄𝐑:- ☞ULLASH × ☆𝐀𝐁𝐇𝐑𝐀𝐍𝐈𝐋☆\n\n𝚈𝚘𝚞 𝙲𝚊𝚗 𝙲𝚊𝚕𝚕 𝙷𝚒𝚖 ULLASH ッ\n😳𝐇𝐢𝐬 𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐢𝐝:- ☞ www.facebook.com/100000654976375\n
-For Any Kind Of Help Contact On massager×✓ m.me/100000654976375 😇`)
+        return out(`This Is My Prefix ⇉ [ ${prefix} ]\n\n𝐎𝐖𝐍𝐄𝐑:- 𝗠𝗥. 𝗣𝟰𝗥𝟰𝗗𝟬𝗫\n\n𝚈𝚘𝚞 𝙲𝚊𝚗 𝙲𝚊𝚕𝚕 𝙷𝚒𝚖 𝗠𝗥. 𝗣𝟰𝗥𝟰𝗗𝟬𝗫\n😳𝐇𝐢𝐬 𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐢𝐝:- ☞ www.facebook.com/100057845471745\n
+For Any Kind Of Help Contact On massager×✓ m.me/100057845471745`)
       }
-      else return out('️️️️️️️️️️️️️️️️️️️️️️️️️️️This Is My Prefix ⇉ [ ${prefix} ]  \n\n𝐎𝐖𝐍𝐄𝐑:- ☞ULLASH × ☆𝐀𝐁𝐇𝐑𝐀𝐍𝐈𝐋☆\n\n𝚈𝚘𝚞 𝙲𝚊𝚗 𝙲𝚊𝚕𝚕 𝙷𝚒𝚖 ULLASH ッ\n😳𝐇𝐢𝐬 𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐢𝐝:- ☞ www.facebook.com/100086680386976\nFor Any Kind Of Help Contact On massager×✓ m.me/100000654976375 ' + data.PREFIX)
+      else return out('️️️️️️️️️️️️️️️️️️️️️️️️️️️This Is My Prefix ⇉ [ ${prefix} ]  \n\n𝐎𝐖𝐍𝐄𝐑:- 𝗠𝗥. 𝗣𝟰𝗥𝟰𝗗𝟬𝗫\n\n𝚈𝚘𝚞 𝙲𝚊𝚗 𝙲𝚊𝚕𝚕 𝙷𝚒𝚖 𝗠𝗥. 𝗣𝟰𝗥𝟰𝗗𝟬𝗫\n😳𝐇𝐢𝐬 𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐢𝐝:- ☞ www.facebook.com/100057845471745\nFor Any Kind Of Help Contact On massager×✓ m.me/100057845471745 ' + data.PREFIX)
     }
 
   });
